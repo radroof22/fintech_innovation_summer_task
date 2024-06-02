e24ed5e161380cf9db7016b01ec19ccf66a7bc78
Rohan Mehta <rmehta98@gatech.edu>
2024-06-02 19:30:56 -0400

    add docs to llama file

--- llm.py
+++ llm.py
@@ -8,9 +8,22 @@ JUMP_SIZE = 4080
 RATE_LIMITING = 0.50
 
 def ask_llama(messages: list[dict[str, str]]) -> str:
+    """
+    Uses DeepInfra API to make a request to Llama 70B-Instruct. Use roles "system", "user", and "assistant".
+        Ensure that the last message is "role: user" and "content: ..." is the question or task for llama.
+
+    Args:
+    - messages: list[dict[str, str]]
+        The input should be a JSON (Python dictionary) containing the relevant chat history
+        the model should use to output its next response. 
+
+    Returns:
+    - string of Llama's response to your last question.
+    """
+
     headers = {"Content-Type": "application/json", "Authorization": f"Bearer {DEEP_INFRA_TOKEN}"}
     data = {
-        "model": "meta-llama/Meta-Llama-3-8B-Instruct",
+        "model": "meta-llama/Meta-Llama-3-70B-Instruct",
         'messages': messages
     }
     resp = requests.post("https://api.deepinfra.com/v1/openai/chat/completions", json=data, headers=headers).json()
@@ -21,6 +34,16 @@ def ask_llama(messages: list[dict[str, str]]) -> str:
     return llama_response
 
 def summarize_operating_segments(llama_responses: list[str]):
+    """
+    Use in case context for llama is too large and multiple requests have to be made to llama for the same task.
+    This function will try to take llama's response and ask Llama to summarize and consolidate the total list of 
+    operating segments that the model proposed.
+
+    Args:
+    - llama_responses: list[str]
+        List of string responses from Llama for the Operating Segment extraction task.
+    """
+
     request_body = [
         {
             "role": "system",
@@ -44,6 +67,24 @@ def summarize_operating_segments(llama_responses: list[str]):
     return response
 
 def identify_operating_segments(context: str, company: str, year: int) -> str:
+    """
+    Given certain elements of the 10-K report, this function will ask Llama to identify the operating 
+    segments for the company. If the context is too large. the function will use multiple requests to llama
+    and then a final request to the model to consolidate the potentially multiple different operating segments
+    it proposed.
+
+    Args:
+        - context: str
+            The specific sections of 10-K report important for identifying the Operating Segments
+        - company: str
+            The name of the company the 10-K report is of
+        - year: str
+            The year the 10-K report was published.
+
+    Returns:
+        - string
+            Llama's raw resposne to trying to identify the operating_segments
+    """
     EXTRACT_OPERATING_SEGMENTS_PROMPT = f"""Read the above sections of the 10-K report for {company} in the year {year}. 
         Identify the stock's operating segments and list them outputting only the operating segments and in the following format: 
         'SEGMENT 1, SEGMENT 2, SEGMENT 3...'"""
@@ -58,7 +99,7 @@ def identify_operating_segments(context: str, company: str, year: int) -> str:
             },
             {
                 "role": "user",
-                "content": context
+                "content": context_chunk
             },
             {
                 "role": "assistant",
@@ -80,10 +121,32 @@ def identify_operating_segments(context: str, company: str, year: int) -> str:
     return responses[0]
 
 
-def identify_revenue_by_operating_segment(context: str, company: str, year: int, operating_segment: str) -> str:
+def identify_operating_income_by_operating_segment(context: str, metric_10k: str, company: str, year: int, operating_segment: str) -> str:
+    """
+    Given relevant sections of the 10-K report for the metric, ask llama to identify the specific metric for the specific operating segment.
+    The metrics include revenue/sales, operating income, and more.
+    In case the context is too large, multiple requets with sections of the context will be used and then Llama
+    will be asked to consolidate its responses into a single response.
+
+    Args:
+        - context: str
+            The specific sections of 10-K report important for identifying the Operating Segments
+        - metric_10k: str
+            The metric to find for each of the 10-K reports
+        - company: str
+            The name of the company the 10-K report is of
+        - year: str
+            The year the 10-K report was published.
+
+    Returns:
+        - string
+            Llama's raw resposne to trying to identify the operating_segments
+
+    """
+    
     EXTRACT_REVENUE_BY_OPERATING_SEGMENT_PROMPT = f"""
         Read the above sections of the 10-K report for {company} in the year {year} relevant to the {operating_segment} operating segment. 
-            Identify the stock's operating income for the {operating_segment} operating segment in the year {year - 1}
+            Identify the stock's {metric_10k} for the {operating_segment} operating segment in the year {year - 1}
             The expected format is below (ONLY OUTPUT THE NUMBER AND DOLLAR SIGN. NO OTHER TEXT): 
             $xxxxxxxx
     """
@@ -98,7 +161,7 @@ def identify_revenue_by_operating_segment(context: str, company: str, year: int,
             },
             {
                 "role": "user",
-                "content": context
+                "content": context_chunk
             },
             {
                 "role": "assistant",
@@ -115,12 +178,31 @@ def identify_revenue_by_operating_segment(context: str, company: str, year: int,
         responses.append(llama_response)
 
     if len(responses) > 1:
-        return consolidate_revenue_by_operating_segment_responses(responses, operating_segment, year)
+        return consolidate_metric_by_operating_segment_responses(responses, metric_10k, operating_segment, year)
     elif len(responses) == 0:
         return ""
     return responses[0]
 
-def consolidate_revenue_by_operating_segment_responses(responses: list[str], operating_segment: str, year: int):
+def consolidate_metric_by_operating_segment_responses(responses: list[str], metric_10k: str, operating_segment: str, year: int) -> str:
+    """
+    In the event that identifying the metric fo the operating segments requires multiple requests, use this Llama request
+    to figure out what is the true answer based on the llama requests you just made.
+
+    Args:
+    - responses: list[str]
+        Raw llama responses for finding the metric for the operating segments.
+    - metric_10k: str
+        Metric that was attempted to be extracted from the llama responses
+    - operating_segment: str
+        Part of the business the metric is extracted for
+    - year: int
+        Year of the 10-K reports release
+
+    Return:
+    - string of the consolidated response for the metric_10k according to the llama responses.
+
+
+    """
     request_body = [
         {
             "role": "system",
@@ -128,7 +210,7 @@ def consolidate_revenue_by_operating_segment_responses(responses: list[str], ope
         },
         {
             "role": "user",
-            "content": f"I previously asked you to identify the the {operating_segment} operating segment revenue in the year {year - 1} according to the 10-K report. Read your responses below. I will ask you a follow up question after: {'\n'.join(responses)}"
+            "content": f"I previously asked you to identify the the {operating_segment} operating segment {metric_10k} in the year {year - 1} according to the 10-K report. Read your responses below. I will ask you a follow up question after: {'\n'.join(responses)}"
         },
         {
             "role": "assistant",
@@ -136,7 +218,7 @@ def consolidate_revenue_by_operating_segment_responses(responses: list[str], ope
         },
         {
             "role": 'user',
-            "content": f"Clearly identify a single value for the revenue for the {operating_segment} operating_segment revenue. Only output one number and nothing else: Should be in the format: $xxxxxx"
+            "content": f"Clearly identify a single value for the {metric_10k} for the {operating_segment} operating_segment {metric_10k}. Only output one number and nothing else: Should be in the format: $xxxxxx"
         }
     ]
 
